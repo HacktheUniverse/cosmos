01c190ac078b496db3381339485f6bfbe2a77de4
Ben Gundersen <ben@bengundersen.com>
2014-11-08 09:51:02 -0500

    universe size

--- universe-sphere.js
+++ universe-sphere.js
@@ -1,8 +1,7 @@
 var THREE = require('three');
 
 // radius, segmentsWidth, segmentsHeight
-
-
+var sphereGeom =  new THREE.SphereGeometry(9000000, 32, 16); 
 var texture = THREE.ImageUtils.loadTexture( './images/mellinger-optmw.png' );
 var material = new THREE.MeshBasicMaterial({
   map: texture,
@@ -15,7 +14,6 @@ obj.position.set(0, 0, 0);
 
 module.exports = {
   init: function(scaleFactor) {
-    var sphereGeom =  new THREE.SphereGeometry(90000000 * scaleFactor, 32, 16); 
 
     var texture = THREE.ImageUtils.loadTexture( './images/mellinger-optmw.png' );
     var material = new THREE.MeshBasicMaterial({
@@ -26,6 +24,7 @@ module.exports = {
     material.side = THREE.BackSide;
     var obj = new THREE.Mesh( sphereGeom.clone(), material );
     obj.position.set(0, 0, 0);
+    obj.scale.set(scaleFactor);
     return obj;
   }
 };
