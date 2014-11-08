cd719ed6f678dea1822708815070179972cbf86b
Ben Gundersen <ben@bengundersen.com>
2014-11-08 00:21:50 -0500

    canvas

--- index.js
+++ index.js
@@ -9,23 +9,22 @@ var camera, renderer, container, light;
 var universe = require('./universe-sphere.js');
 
 var init = function() {
-  scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-  camera.position.set(0,150,400);
-  //camera.lookAt(scene.position);
-  camera.lookAt(0,0,0);
-
-  scene.add(universe);	
-
-  // LIGHT
-	light = new THREE.PointLight(0xffffff);
-	light.position.set(0,150,100);
-	scene.add(light);
-
   renderer = new THREE.WebGLRenderer();
   container = document.getElementById('container');
   renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
+  container.appendChild( renderer.domElement );
+
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  camera.position.set(0,150,200);
+  //camera.lookAt(scene.position);
+  camera.lookAt(0,0,0);
+  // LIGHT
+	light = new THREE.PointLight(0xffffff);
+	light.position.set(0,150,100);
+
+  scene = new THREE.Scene();
+  scene.add(universe);	
+	scene.add(light);
 
   stats = new Stats();
   stats.domElement.style.position = 'absolute';
