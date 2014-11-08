9fc0282583421c4a223c5ac0175f166ea0be37df
Ben Gundersen <ben@bengundersen.com>
2014-11-07 22:17:56 -0500

    Basic three.js setup

---
+++ index.js
@@ -0,0 +1,9 @@
+var _     = require('lodash');
+var THREE = require('three');
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
