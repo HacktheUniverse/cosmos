393be77daa0ac97502a5e42ee4b63dc175631e2a
Ben Gundersen <ben@bengundersen.com>
2014-11-08 09:30:04 -0500

    Trying to fix star brightness

--- universe-sphere.js
+++ universe-sphere.js
@@ -1,16 +1,20 @@
 var THREE = require('three');
 
 // radius, segmentsWidth, segmentsHeight
-var sphereGeom =  new THREE.SphereGeometry(900000, 32, 16); 
 
-var texture = THREE.ImageUtils.loadTexture( './images/mellinger-optmw.png' );
-var material = new THREE.MeshBasicMaterial({
-  map: texture,
-  transparent: true,
-  opacity: 0.7
-});
-material.side = THREE.BackSide;
-var obj = new THREE.Mesh( sphereGeom.clone(), material );
-obj.position.set(0, 0, 0);
+module.exports = {
+  init: function(scaleFactor) {
+    var sphereGeom =  new THREE.SphereGeometry(900000 * scaleFactor, 32, 16); 
 
-module.exports = obj;
+    var texture = THREE.ImageUtils.loadTexture( './images/mellinger-optmw.png' );
+    var material = new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+      opacity: 0.7
+    });
+    material.side = THREE.BackSide;
+    var obj = new THREE.Mesh( sphereGeom.clone(), material );
+    obj.position.set(0, 0, 0);
+    return obj;
+  }
+};
